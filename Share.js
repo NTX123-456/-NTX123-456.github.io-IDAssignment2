d6d1bad648fbf02f0eda34081e36a7915f9543c7
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-01-03 21:40:01 +0800

    Add files via upload

---
+++ Share.js
@@ -0,0 +1,24 @@
+const inpKey = document.getElementById("inpKey");
+const inpValue = document.getElementById("inpValue");
+const btnsubmit = document.getElementById("btnsubmit");
+const Output = document.getElementById("Output");
+
+localStorage.removeItem("JACk")
+btnsubmit.onclick = function(){
+    const key = inpKey.value;
+    const value = inpValue.value;
+
+   
+
+    if (key && value){
+        localStorage.setItem(key,value);
+        location.reload();
+    }
+};
+
+for (let i = 0; i< localStorage.length; i++){
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    Output.innerHTML += `${key}: ${value}<br/ >`;
+}
