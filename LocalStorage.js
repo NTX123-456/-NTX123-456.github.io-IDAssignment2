fc56f2a0c829fe8da42ad2f747640c3f6bea9816
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-01-08 00:41:47 +0800

    Removed unnecessary line breaks

--- LocalStorage.js
+++ LocalStorage.js
@@ -2,24 +2,20 @@ const inpKey = document.getElementById("inpKey");
 const inpValue = document.getElementById("inpValue");
 const btnsubmit = document.getElementById("btnsubmit");
 const Output = document.getElementById("Output");
-
-
 btnsubmit.onclick = function(){
     const key = inpKey.value;
     const value = inpValue.value;
-
-   
-
-    if (key && value){
+     if (key && value){
         localStorage.setItem(key,value);
         location.reload();
     }
 };
-
 for (let i = 0; i< localStorage.length; i++){
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
-
     Output.innerHTML += `${key}: ${value}<br/ >`;
-    
 }
+   
+   
+
+   
