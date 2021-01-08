834ce7355e84029d354ed085d6141a6216954452
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-01-08 13:32:38 +0800

    Updated back to old version
    as the updated version does not work

--- LocalStorage.js
+++ LocalStorage.js
@@ -1,22 +1,20 @@
-/*jslint browser:true */
-var inpKey = document.getElementById("inpKey"),
-    inpValue = document.getElementById("inpValue"),
-    btnsubmit = document.getElementById("btnsubmit"),
-    Output = document.getElementById("Output");
-btnsubmit.onclick = function () { 'use strict';
-    var key = inpKey.value,
-        value = inpValue.value;
-     if (key && value) {
-        localStorage.setItem(key, value);
+const inpKey = document.getElementById("inpKey");
+const inpValue = document.getElementById("inpValue");
+const btnsubmit = document.getElementById("btnsubmit");
+const Output = document.getElementById("Output");
+btnsubmit.onclick = function(){
+    const key = inpKey.value;
+    const value = inpValue.value;
+     if (key && value){
+        localStorage.setItem(key,value);
         location.reload();
     }
-     };
-for (var i = 0; i< localStorage.length; i++){ 'use strict';
-    var key1 = localStorage.key(i);
-    var value1 = localStorage.getItem(key);
-    Output.innerHTML += `${key1}: ${value1}<br/ >`;
+};
+for (let i = 0; i< localStorage.length; i++){
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    Output.innerHTML += `${key}: ${value}<br/ >`;
 }
-
    
    
 
