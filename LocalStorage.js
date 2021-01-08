6dd47681237ea0adebb25cce96c74784052f15f7
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-01-08 23:13:32 +0800

    Added some comments

--- LocalStorage.js
+++ LocalStorage.js
@@ -1,22 +1,22 @@
-const inpKey = document.getElementById("inpKey");
+const inpKey = document.getElementById("inpKey");    /*Declare Constants*/
 const inpValue = document.getElementById("inpValue");
 const btnsubmit = document.getElementById("btnsubmit");
 const Output = document.getElementById("Output");
-btnsubmit.onclick = function(){
+btnsubmit.onclick = function(){          /*Function that detects for user input*/
     const key = inpKey.value;
     const value = inpValue.value;
      if (key && value){
         localStorage.setItem(key,value);
         location.reload();
-     if (inpKey === '' || inpKey === null) {
+     if (document.getElementById("inpKey") === '' || document.getElementById("inpKey") === null) {
             alert("Please enter a country");
         }
-     if (inpValue === '' || inpValue === null) {
+     if (document.getElementById("inpValue") === '' || document.getElementById("inpValue") === null) {
             alert("Please enter some information");
         }
     }
 };
-for (let i = 0; i< localStorage.length; i++){
+for (let i = 0; i< localStorage.length; i++){   /*Function that prints and displays user input */
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
     Output.innerHTML += `${key}: ${value}<br/ >`;
