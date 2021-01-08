95cd8e5cd82c2e5341da6a69ee84190a41715c09
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-01-08 11:07:10 +0800

    Changed many errors

--- index.js
+++ index.js
@@ -1,120 +1,101 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 /* Initialize Jquery usage */
+/*jslint devel: true */
 
 
 
 
 $.getJSON("https://api.apify.com/v2/key-value-stores/yaPbKe9e5Et61bl7W/records/LATEST?disableRedirect=true",
-   function(data){
-   console.log(data);
-   var infected = data.infected;
-   $(".infected").append(infected);
-   var discharged = data.discharged;
-   $(".discharged").append(discharged);
-   var inCommunityFacilites = data.inCommunityFacilites;
-   $(".Facilities").append(inCommunityFacilites);
-   var deceased = data.deceased
-   $(".deceased").append(deceased);
-   var recovered = data.recovered
-   $(".recovered").append(recovered);
-   var activeCases = data.activeCases
-   $(".activeCases").append(activeCases);
-   var criticalHospitalized = data.criticalHospitalized
-   $(".criticalHospitalized").append(criticalHospitalized);
-   }
-);
+     function (data) { 'use strict';
+        console.log(data);
+        var infected = data.infected,
+            discharged = data.discharged,
+            inCommunityFacilites = data.inCommunityFacilites,
+            deceased = data.deceased,
+            recovered = data.recovered,
+            activeCases = data.activeCases,
+            criticalHospitalized = data.criticalHospitalized;
+            $(".infected").append(infected);
+            $(".discharged").append(discharged);
+            $(".Facilities").append(inCommunityFacilites);
+            $(".deceased").append(deceased);
+            $(".recovered").append(recovered);
+            $(".activeCases").append(activeCases);
+            $(".criticalHospitalized").append(criticalHospitalized);
+         }
+     );
 $.getJSON("https://corona.lmao.ninja/v2/all?yesterday",
-   function(data){
-   console.log(data);
-   var affectedCountries = data.affectedCountries
-   $(".affectedCountries").append(affectedCountries);
-   var cases = data.cases
-   $(".cases").append(cases);
-   var todayCases = data.todayCases
-   $(".todayCases").append(todayCases);
-   var deaths = data.deaths
-   $(".deaths").append(deaths);
-   var todayDeaths = data.todayDeaths
-   $(".todayDeaths").append(todayDeaths);
-   var recovered = data.recovered
-   $(".recovered2").append(recovered);
-   }
-);
-function changeurl() {
-   location.replace("https://www.moh.gov.sg/covid-19")
- }
-function changeurl2(){
-   location.replace("https://www.worldometers.info/coronavirus/")
-}
-function changeurl3(){
-   location.replace("https://www.who.int/health-topics/coronavirus#tab=tab_1")
-}
-function changeurl4(){
-   location.replace("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public")
-}
-function changeurl5(){
-   location.replace("https://www.channelnewsasia.com/news/topics/coronavirus-covid-19?cid=cna-sghome_covid19_outbreak_desktop_11032020_cna")
-}
-function changeurl6(){
-   location.replace("https://www.tracetogether.gov.sg/")
-}
-function changeurl7(){
-   location.replace("https://www.moh.gov.sg/policies-and-legislation/covid-19-(temporary-measures)-(control-order)-regulations")
-}
-function changeurl8(){
-   location.replace("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19#:~:text=symptoms")
-}
+         function (data) { 'use strict';
+         console.log(data);
+         var affectedCountries = data.affectedCountries,
+            cases = data.cases,
+            todayCases = data.todayCases,
+            deaths = data.deaths,
+            todayDeaths = data.todayDeaths,
+            recovered = data.recovered;
+            $(".affectedCountries").append(affectedCountries);
+            $(".cases").append(cases);
+            $(".todayCases").append(todayCases);
+            $(".deaths").append(deaths);
+            $(".todayDeaths").append(todayDeaths);
+            $(".recovered2").append(recovered);
+        }
+     );
+
+/*jslint indent: 4 */
+/*global plugin, data, $, i, dataLoadedEvent: true,myFunction */
+
+
 var api = "https://corona.lmao.ninja/v2/countries/";
 var apiend = "?yesterday'?yesterday=true&strict=true&query";
 var input;
-function myFunction() {
-   var request;
-   var input1 = document.getElementById('country');
-   var api = 'https://corona.lmao.ninja/v2/countries/';
-   var apikey =
-       "?yesterday'?yesterday=true&strict=true&query";
-   var sum = api + input1.value + apikey;
-   request = new XMLHttpRequest();
-   request.open('GET', sum, true);
-   request.onload = function () {
-       var data = JSON.parse(this.response);
-       if (request.status >= 200 && request.status < 400) {
-           console.log(data);
-           var population = data.population
-           $(".population").append(population);
-           var active = data.active
-           $(".active").append(active);
-           var activePerOneMillion = data.activePerOneMillion
-           $(".activePerOneMillion").append(activePerOneMillion);
-           var totalcases = data.cases
-           $(".totalcases").append(totalcases);
-           var totalcasesPerOneMillion  = data.casesPerOneMillion
-           $(".totalcasesPerOneMillion").append(totalcasesPerOneMillion);
-           var critical  = data.critical
-           $(".critical").append(critical);
-           var deaths  = data.deaths
-           $(".deaths2").append(deaths);
-           var todayCases  = data.todayCases
-           $(".todayCases2").append(todayCases);
-           var todayDeaths  = data.todayDeaths
-           $(".todayDeaths2").append(todayDeaths);
-       } else {
-           console.log(input1.value);
-       }
-   }
-   request.send();
-}
-function clearBox()
-{
-   document.getElementById("Reset");
-   window.location.reload();
-}    
-          
-   
-       
-     
-   
+function myFunction() { 'use strict';
+    var request,
+        input1 = document.getElementById('country'),
+        api = 'https://corona.lmao.ninja/v2/countries/',
+        apikey =
+            "?yesterday'?yesterday=true&strict=true&query",
+        sum = api + input1.value + apikey;
+    request = new XMLHttpRequest();
+    request.open('GET', sum, true);
+    request.onload = function () {
+        var data = JSON.parse(this.response),
+            population = data.population,
+            active = data.active,
+            activePerOneMillion = data.activePerOneMillion,
+            totalcasesPerOneMillion = data.casesPerOneMillion,
+            totalcases = data.cases,
+            critical = data.critical,
+            deaths = data.deaths,
+            todayCases  = data.todayCases,
+            todayDeaths  = data.todayDeaths;
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data);
+            $(".population").append(population);
+            $(".active").append(active);
+            $(".activePerOneMillion").append(activePerOneMillion);
+            $(".totalcases").append(totalcases);
+            $(".totalcasesPerOneMillion").append(totalcasesPerOneMillion);
+            $(".critical").append(critical);
+            $(".deaths2").append(deaths);
+            $(".todayCases2").append(todayCases);
+            $(".todayDeaths2").append(todayDeaths);
+        } else {
+            console.log(input1.value);
+        }
+    };
+     }
+function clearBox() {'use strict';
+    document.getElementById("Reset");
+    window.location.reload();
+    }
+
+
+
+
+
+
   
 
 
