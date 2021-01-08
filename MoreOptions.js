2575b8f9b528ac1b4d83bab9834ddfd5715c463d
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-01-08 12:24:52 +0800

    Updated most major errors

--- MoreOptions.js
+++ MoreOptions.js
@@ -1,115 +1,117 @@
-function myFunction() {
-    var request;
-    var input1 = document.getElementById('continent');
-    var api = 'https://corona.lmao.ninja/v2/continents/';
-    var apikey =
-        "?yesterday&stricty";
-    var sum = api + input1.value + apikey;
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/* Initialize Jquery usage */
+/*jslint devel: true */
+
+
+
+
+
+function myFunction() { 'use strict';
+    var request,
+        input1 = document.getElementById('continent'),
+        api = 'https://corona.lmao.ninja/v2/continents/',
+        apikey =
+            "?yesterday&stricty",
+        sum = api + input1.value + apikey;
+    request = new XMLHttpRequest();
+    request.open('GET', sum, true);
+    request.onload = function () {
+        var data = JSON.parse(this.response),
+            active = data.active,
+            continent = data.continent,
+            activePerOneMillion = data.activePerOneMillion,
+            cases = data.cases,
+            countries = data.countries,
+            deaths = data.deaths,
+            recovered = data.recovered,
+            critical = data.critical;
+        $(".active").append(active);
+        $(".continent").append(continent);
+        $(".activePerOneMillion").append(activePerOneMillion);
+        $(".cases").append(cases);
+        $(".countries").append(countries);
+        $(".deaths").append(deaths);
+        $(".recovered").append(recovered);
+        $(".critical").append(critical);
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data);
+        } else {
+            console.log(input1.value);
+        }
+    };
+     }
+function clearBox() { 'use strict';
+    document.getElementById("Reset");
+    window.location.reload();
+     }
+function myFunction2() { 'use strict';
+    var request,
+        input1 = document.getElementById('countries'),
+        api = 'https://corona.lmao.ninja/v2/countries/',
+        apikey =
+            "?yesterday",
+        sum = api + input1.value + apikey;
+    request = new XMLHttpRequest();
+    request.open('GET', sum, true);
+    request.onload = function () {
+        var data = JSON.parse(this.response),
+            country = data[0].country,
+            active = data[0].active,
+            activePerOneMillion = data[0].activePerOneMillion,
+            cases = data[0].cases,
+            continent = data[0].continent,
+            country2 = data[1].country,
+            activePerOneMillion2 = data[1].activePerOneMillion,
+            cases2 = data[1].cases,
+            active2 = data[1].active,
+            continent2 = data[1].continent;
+        $(".country1").append(country);
+        $(".active1").append(active);
+        $(".activePerOneMillion1").append(activePerOneMillion);
+        $(".cases1").append(cases);
+        $(".continent1").append(continent);
+        $(".country2").append(country2);
+        $(".active2").append(active2);
+        $(".activePerOneMillion2").append(activePerOneMillion2);
+        $(".cases2").append(cases2);
+        $(".continent2").append(continent2);
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data);
+        } else {
+            console.log(input1.value);
+        }
+    };
+     }
+function clearBox2() { 'use strict';
+    document.getElementById("Reset");
+    window.location.reload();
+     }
+function myFunction3() { 'use strict';
+    var request,
+        input1 = document.getElementById('Historical'),
+        input2 = document.getElementById('days'),
+        api = 'https://corona.lmao.ninja/v2/historical/',
+        apikey =
+            "?lastdays=",
+        sum = api + input1.value + apikey + input2.value;
     request = new XMLHttpRequest();
     request.open('GET', sum, true);
     request.onload = function () {
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
             console.log(data);
-            var active = data.active
-            $(".active").append(active);
-            var continent = data.continent
-            $(".continent").append(continent);
-            var activePerOneMillion = data.activePerOneMillion
-            $(".activePerOneMillion").append(activePerOneMillion);
-            var cases = data.cases
-            $(".cases").append(cases);
-            var countries = data.countries
-            $(".countries").append(countries);
-            var deaths = data.deaths
-            $(".deaths").append(deaths);
-            var recovered = data.recovered
-            $(".recovered").append(recovered);
-            var critical = data.critical
-            $(".critical").append(critical);
         } else {
             console.log(input1.value);
         }
+    };
     }
-    request.send();
- }
-function clearBox()
-{
-   document.getElementById("Reset");
-   window.location.reload();
-}
-function myFunction2() {
-    var request;
-    var input1 = document.getElementById('countries');
-    var api = 'https://corona.lmao.ninja/v2/countries/';
-    var apikey =
-        "?yesterday";
-    var sum = api + input1.value + apikey;
-    request = new XMLHttpRequest();
-    request.open('GET', sum, true);
-    request.onload = function () {
-        var data = JSON.parse(this.response);
-        if (request.status >= 200 && request.status < 400) {
-            console.log(data);
-            var country = data[0].country
-            $(".country1").append(country);
-            var active = data[0].active
-            $(".active1").append(active)
-            var activePerOneMillion = data[0].activePerOneMillion
-            $(".activePerOneMillion1").append(activePerOneMillion)
-            var cases = data[0].cases
-            $(".cases1").append(cases)
-            var continent = data[0].continent
-            $(".continent1").append(continent)
-            var country = data[1].country
-            $(".country2").append(country);
-            var active = data[1].active
-            $(".active2").append(active)
-            var activePerOneMillion = data[1].activePerOneMillion
-            $(".activePerOneMillion2").append(activePerOneMillion)
-            var cases = data[1].cases
-            $(".cases2").append(cases)
-            var continent = data[1].continent
-            $(".continent2").append(continent)
-        } else {
-            console.log(input1.value);
-        }
-    }
-    request.send();
- }
-function clearBox2()
-{
-   document.getElementById("Reset");
-   window.location.reload();
-}
-function myFunction3() {
-    var request;
-    var input1 = document.getElementById('Historical');
-    var input2 = document.getElementById('days');
-    var api = 'https://corona.lmao.ninja/v2/historical/';
-    var apikey =
-        "?lastdays=";
-    var sum = api + input1.value + apikey + input2.value;
-    request = new XMLHttpRequest();
-    request.open('GET', sum, true);
-    request.onload = function () {
-        var data = JSON.parse(this.response);
-        if (request.status >= 200 && request.status < 400) {
-            console.log(data);
-        } else {
-            console.log(input1.value);
-        }
-    }
-    request.send();
- }
-function clearBox3()
-{
-   document.getElementById("Reset");
-   window.location.reload();
-}              
-   
- 
-       
+function clearBox3() { 'use strict';
+    document.getElementById("Reset");
+    window.location.reload();
+     }
+
+
      
         
     
